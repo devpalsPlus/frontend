5d7e401a98c8a8bd4df2e2771840738ff6e850d7
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2025-01-04 00:29:49 +0900

    [Feat] 프로젝트 수 ui

--- src/components/home/projectStats/ProjectStats.styled.ts
+++ src/components/home/projectStats/ProjectStats.styled.ts
@@ -1,3 +1,14 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 343px;
+  display: flex;
+  align-items: center;
+`;
+export const Wrapper = styled.div`
+  display: flex;
+  width: 1100px;
+  height: 116px;
+  margin: 0 auto;
+  gap: 100px;
+`;
--- src/components/home/projectStats/ProjectStats.tsx
+++ src/components/home/projectStats/ProjectStats.tsx
@@ -1,9 +1,15 @@
+import { PROJECT_STATS_TITLE } from '../../../constants/homeConstants';
+import ProjectStat from './projectStat/ProjectStat';
 import * as S from './ProjectStats.styled';
 
 export default function ProjectStats() {
   return (
     <S.Container>
-      <h1>ProjectStats</h1>
+      <S.Wrapper>
+        {PROJECT_STATS_TITLE.map((title) => (
+          <ProjectStat title={title} />
+        ))}
+      </S.Wrapper>
     </S.Container>
   );
 }
