4c00ef6652eb9c362d0bfe9290f0a8977cfcbff5
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-01-06 17:31:09 +0900

    refactor : 파일 위치 변경

--- src/components/createProjectComponents/markdownEditor/MdEditorInput.tsx
+++ src/components/createProjectComponents/markdownEditor/MdEditorInput.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import * as S from './MdEditorInput.styled';
-import MDEditor from '@uiw/react-md-editor';
 
 const MdEditorInput = ({ field }) => {
   return (
--- src/components/createProjectComponents/inputComponent.tsx
+++ src/components/createProjectComponents/inputComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Control, Controller, FieldErrors } from 'react-hook-form';
-import * as S from '../pages/createProject/CreateProject.styled';
-import MdEditorInput from './markdownEditor/MdEditorInput';
+import * as S from '../../pages/createProject/CreateProject.styled';
+import MdEditorInput from '../markdownEditor/MdEditorInput';
 
 type InputProps = {
   control: Control<any>;
@@ -32,14 +32,7 @@ const Input = ({
   const renderInput = (field: any) => {
     switch (type) {
       case 'date':
-        return (
-          <S.DateInput
-            {...field}
-            name={name}
-            type="date"
-            placeholder={placeholder}
-          />
-        );
+        return <S.DateInput {...field} type="date" placeholder={placeholder} />;
 
       case 'textarea':
         return <S.TextArea {...field} placeholder={placeholder} />;
@@ -56,12 +49,7 @@ const Input = ({
     switch (type) {
       case 'text':
         return (
-          <S.InfoInputText
-            {...field}
-            name={name}
-            type="text"
-            placeholder={placeholder}
-          />
+          <S.InfoInputText {...field} type="text" placeholder={placeholder} />
         );
       case 'select':
         return (
@@ -73,12 +61,7 @@ const Input = ({
         );
       case 'number':
         return (
-          <S.InfoInputText
-            {...field}
-            name={name}
-            type="number"
-            placeholder={placeholder}
-          />
+          <S.InfoInputText {...field} type="number" placeholder={placeholder} />
         );
       case 'checkbox':
         return <S.InfoInputCheckbox {...field} type="checkbox" id={name} />;
