e46285e6a898e0f5dda5a2c2e498bd720dc20841
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2025-01-06 09:35:59 +0900

    [Fix] 프로젝트 수 map에 key prop 추가

--- src/components/home/projectStats/ProjectStats.tsx
+++ src/components/home/projectStats/ProjectStats.tsx
@@ -1,13 +1,14 @@
 import { PROJECT_STATS_TITLE } from '../../../constants/homeConstants';
 import ProjectStat from './projectStat/ProjectStat';
 import * as S from './ProjectStats.styled';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function ProjectStats() {
   return (
     <S.Container>
       <S.Wrapper>
         {PROJECT_STATS_TITLE.map((title) => (
-          <ProjectStat title={title} />
+          <ProjectStat title={title} key={uuidv4()} />
         ))}
       </S.Wrapper>
     </S.Container>
