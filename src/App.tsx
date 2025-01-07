ea27ab29a420a0dd7e9ff47a044f360262642558
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-01-07 18:24:32 +0900

    chore: mock서버 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './style/theme';
+import { GlobalStyle } from './style/global';
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <AppRoutes />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { GlobalStyle } from './style/global';
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './style/theme.ts';
 
-createRoot(document.getElementById('root')!).render(
-  <>
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
+async function mountApp() {
+  if (process.env.NODE_ENV === 'development') {
+    const { worker } = await import('./mock/browser');
+    await worker.start();
+  }
+
+  createRoot(document.getElementById('root')!).render(
+    <>
       <App />
-    </ThemeProvider>
-  </>
-);
+    </>
+  );
+}
+
+mountApp();
