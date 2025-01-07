1e6c000a4fb0c6ba41f7ff5862c805d6fa02643b
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-01-07 18:27:52 +0900

    feat: 모집 등록한 프로젝트 리스트 모델 추가

---
+++ src/models/manageMyProjectList.ts
@@ -0,0 +1,10 @@
+export interface ManageMyprojectList {
+  title: string;
+  description: string;
+  totalMember: number;
+  startDate: string;
+  positionId: number;
+  estimatedPeriod: string;
+  methodId: number;
+  isBeginner: boolean;
+}
