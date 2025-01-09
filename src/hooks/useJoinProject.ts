9b3f99c6c7c1c1ae0d557cecdafd8c59e6df7d99
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-01-10 02:04:41 +0900

    feat : useParam id로 프로젝트 데이터 가져오기

---
+++ src/hooks/useJoinProject.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+import { getProjectData } from '../api/joinProject.ap';
+
+const useJoinProject = (id: number) => {
+  const { data, isLoading, isFetching } = useQuery({
+    queryKey: ['joinProject'],
+    queryFn: async () => await getProjectData(id),
+    staleTime: 1000 * 60 * 5,
+  });
+
+  return { data, isLoading, isFetching };
+};
+
+export default useJoinProject;
