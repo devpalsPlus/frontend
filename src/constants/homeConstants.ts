9ce222878dc24e03b410bdb732ec6241937633d5
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-01-09 17:07:34 +0900

    feat : 사용 언어 카테고리 스타일 SkillTagBox로 변경

--- src/constants/homeConstants.ts
+++ src/constants/homeConstants.ts
@@ -23,10 +23,26 @@ export const PROJECT_SKILL = [
 
 export const PROJECT_POSITION = [
   '전체',
-  '백엔드',
-  '프론트엔드',
-  'PM',
   '디자이너',
+  '프론트엔드',
+  '백엔드',
+  '데이터 분석가',
+  '기획자',
+  '모바일 개발자',
+  'AI/ML 엔지니어',
+  'DevOps 엔지니어',
+  '게임 개발자',
+  'UI/UX 디자이너',
+  '풀스택 개발자',
+  '마케팅 전문가',
+  '제품 관리자 (PM)',
+  'QA 엔지니어',
+  '블록체인 개발자',
+  '사이버 보안 전문가',
+  'AR/VR 개발자',
+  '사운드 디자이너',
+  '영상/미디어 전문가',
+  '기술 작가',
 ] as const;
 
 export const PROJECT_METHOD = [
