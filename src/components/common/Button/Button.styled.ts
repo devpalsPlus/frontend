1057a6cc46e29f80e87e06801d1ec97f4fff21ce
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-01-10 15:46:38 +0900

    feat: 버튼 공통 컴포넌트 구현

---
+++ src/components/common/Button/Button.styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { ButtonProps } from './Button';
+
+export const CommonButton = styled.button<Omit<ButtonProps, 'children'>>`
+  font-size: ${({ theme, size }) => theme.buttonSize[size].fontSize};
+  padding: ${({ theme, size }) => theme.buttonSize[size].padding};
+  color: ${({ theme, schema }) => theme.buttonScheme[schema].color};
+  background-color: ${({ theme, schema }) => theme.buttonScheme[schema].bg};
+  border-radius: ${({ theme, size }) => theme.borderRadius[size]};
+  pointer-events: ${({ disabled }) => (disabled ? 'none' : 'auto')};
+  cursor: ${({ disabled }) => (disabled ? 'none' : 'pointer')};
+`;
