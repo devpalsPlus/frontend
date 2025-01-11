463860b882df11f6a694acaeb0f2d6965a848d1f
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-01-11 22:37:13 +0900

    feat : 프로젝트 상세 페이지 90% 구현

--- src/pages/projectDetail/ProjectDetail.tsx
+++ src/pages/projectDetail/ProjectDetail.tsx
@@ -1,34 +1,58 @@
+import { useNavigate, useParams } from 'react-router-dom';
 import ProjectInformation from '../../components/createProjectComponents/ProjectInformation';
+import useGetProjectData from '../../hooks/useJoinProject';
 import * as S from './ProjectDetail.styled';
+import { formatDate } from '../../util/format';
+import Button from '../../components/common/Button/Button';
 
 const ProjectDetail = () => {
+  const { projectId } = useParams();
+  const id = Number(projectId);
+  const navigate = useNavigate();
+  const { data, isLoading, isFetching } = useGetProjectData(id);
+
+  if (!data) {
+    return <div>데이터가 없습니다.</div>;
+  }
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isFetching) return <div>isFetching...</div>;
+
+  const handleApplyClick = () => {
+    navigate(`/main/apply/${id}`);
+  };
+
   return (
     <S.Container>
       <S.Header>
-        <S.ProfileImage src='/path/to/profile-image.jpg' alt='profile' />
-        <S.UserInfo>
-          <S.UserName>Jenny</S.UserName>
-          <S.PostDate>2024. 12. 29</S.PostDate>
-          <S.ViewCount>👁️ 1</S.ViewCount>
-        </S.UserInfo>
+        <S.Title>{data.title}</S.Title>
+        <S.ProfileContainer>
+          <S.ProfileImage src={data.User.profileImg} alt='profile' />
+          <S.UserInfo>
+            <S.UserName>{data.User.nickname}</S.UserName>
+            <S.PostDate>{formatDate(data.recruitmentEndDate)}</S.PostDate>
+            <S.ViewCount>👁️{data.views}</S.ViewCount>
+          </S.UserInfo>
+        </S.ProfileContainer>
       </S.Header>
 
       <S.Content>
-        <ProjectInformation />
+        <ProjectInformation data={data} />
 
         <S.ProjectDescription>
           <S.SectionTitle>프로젝트 소개</S.SectionTitle>
-          <S.Description>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
-            Interdum sit ac risus venenatis tellus dolor donec in. <br />
-            <br />
-            {`Et gravida eget platea quis non imperdiet convallis. Nunc scelerisque pharetra nisl.`}
-            ...
-          </S.Description>
+          <S.Description>{data.description}</S.Description>
         </S.ProjectDescription>
       </S.Content>
 
-      <S.ActionButton>프로젝트 함께하기</S.ActionButton>
+      <Button
+        size='primary'
+        schema='primary'
+        radius='primary'
+        onClick={handleApplyClick}
+      >
+        프로젝트 함께하기
+      </Button>
     </S.Container>
   );
 };
