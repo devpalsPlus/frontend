a4a5503ec5ffe39c9cc1429877c651c1358e7146
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-01-15 00:19:06 +0900

    refactor: API모델 변경에 따른 SkillTag필드 추가

--- src/components/common/avatar/AvartarList.tsx
+++ src/components/common/avatar/AvartarList.tsx
@@ -1,18 +1,17 @@
 import * as S from './AvatarList.styled';
-import { SkillTag } from '../../../models/manageMyProject';
+import { ProjectSkillTag } from '../../../models/manageMyProject';
 import Avatar from './Avatar';
 interface AvartarListProps {
-  avatars: SkillTag[] | null;
+  avatars: ProjectSkillTag[] | null;
   size?: string;
 }
 
 function AvartarList({ avatars, size = '33px' }: AvartarListProps) {
   if (!avatars) return;
-
   return (
     <S.Wrapper>
       {avatars.map((avatar) => (
-        <Avatar size={size} image={avatar.img} />
+        <Avatar size={size} image={avatar.SkillTag.img} />
       ))}
     </S.Wrapper>
   );
