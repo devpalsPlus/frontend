5391887e92aa1aabd06cac5a609f00bbe556c450
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-01-15 22:15:57 +0900

    feat: query gcTime 20000으로 기본값 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './style/global';
 import { defaultTheme } from './style/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 20000,
+    },
+  },
+});
 
 function App() {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        gcTime: 20000,
-      },
-    },
-  });
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
