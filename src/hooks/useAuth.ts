bc77e69020ee66010c4c1a3b18a3dd5269d57b36
HongbiKo <rhghdql666@gmail.com>
2025-01-16 23:18:46 +0900

    refactor: 로그인,회원가입,비밀번호재설정 tanstackquery적용

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -5,43 +5,84 @@ import { changePasswordFormValues } from '../pages/changePassword/ChangePassword
 import { loginFormValues } from '../pages/login/Login';
 import useAuthStore from '../store/authStore';
 import { useAlert } from './useAlert';
+import { useMutation } from '@tanstack/react-query';
+import { LoginResponse } from '../models/auth';
 
 export const useAuth = () => {
   const navigate = useNavigate();
   const { storeLogin, storeLogout } = useAuthStore();
   const { showAlert } = useAlert();
 
+  const signupMutaton = useMutation<
+    void,
+    Error,
+    { email: string; password: string; nickname: string }
+  >({
+    mutationFn: async ({ email, password, nickname }) => {
+      await postSignUp({ email, password, nickname });
+    },
+    onSuccess: () => {
+      showAlert('회원가입 완료되었습니다.');
+      navigate('/login');
+    },
+    onError: () => {
+      showAlert('회원가입 실패하였습니다.');
+    },
+  });
+
+  const resetPasswordMutation = useMutation<
+    void,
+    Error,
+    { email: string; newPassword: string }
+  >({
+    mutationFn: async ({ email, newPassword }) => {
+      await postResetPassword({ email, newPassword });
+    },
+    onSuccess: () => {
+      showAlert('비밀번호가 성공적으로 재설정 되었습니다.');
+      navigate('/login');
+    },
+    onError: () => {
+      showAlert('비밀번호 재설정에 실패하였습니다.');
+    },
+  });
+
+  const userLoginMutation = useMutation<
+    LoginResponse,
+    Error,
+    { email: string; password: string }
+  >({
+    mutationFn: async ({ email, password }) => {
+      const response = await postLogin({ email, password });
+      const { accessToken, refreshToken } = response.data;
+      return { accessToken, refreshToken };
+    },
+    onSuccess: (data) => {
+      const { accessToken, refreshToken } = data;
+
+      showAlert('로그인 되었습니다.');
+      storeLogin(accessToken, refreshToken);
+      navigate('/main');
+    },
+    onError: () => {
+      showAlert('가입되지 않은 계정입니다.');
+    },
+  });
+
   const userSignup = (
     data: Pick<registerFormValues, 'email' | 'password' | 'nickname'>
   ) => {
-    postSignUp(data).then(() => {
-      showAlert('회원가입 완료되었습니다.');
-      navigate('/login');
-    });
+    signupMutaton.mutate(data);
   };
 
   const resetPassword = (
     data: Pick<changePasswordFormValues, 'email' | 'newPassword'>
   ) => {
-    postResetPassword(data).then(() => {
-      showAlert('비밀번호가 성공적으로 재설정 되었습니다.');
-      navigate('/login');
-    });
+    resetPasswordMutation.mutate(data);
   };
 
   const userLogin = (data: loginFormValues) => {
-    postLogin(data).then(
-      (res) => {
-        showAlert('로그인 되었습니다.');
-        storeLogin(res.data.accessToken, res.data.refreshToken);
-        navigate('/main');
-      },
-      (error) => {
-        if (error) {
-          showAlert('가입되지 않은 계정입니다.');
-        }
-      }
-    );
+    userLoginMutation.mutate(data);
   };
 
   const userLogout = () => {
