3bdadfc349d3dbb1053ce35845432762cf16c5f8
HongbiKo <rhghdql666@gmail.com>
2025-01-16 22:08:14 +0900

    feat: 사이드바 생성

--- src/components/common/sidebar/Sidebar.tsx
+++ src/components/common/sidebar/Sidebar.tsx
@@ -1,7 +1,38 @@
+import { NavLink } from 'react-router-dom';
+import Avatar from '../avatar/Avatar';
 import * as S from './Sidebar.styled';
 
-const Sidebar = () => {
-  return <S.Container></S.Container>;
+interface MenuItem {
+  label: string;
+  path: string;
+  icon?: React.ReactNode;
+}
+
+interface SidebarProps {
+  menuItems: MenuItem[];
+  profileImage?: string;
+  nickname?: string;
+}
+
+const Sidebar = ({ menuItems, profileImage, nickname }: SidebarProps) => {
+  return (
+    <S.Container>
+      <S.AvartarWrapper>
+        <Avatar size='120px' image={profileImage} />
+        <span>{nickname ? nickname : ''}</span>
+      </S.AvartarWrapper>
+      <S.MenuList>
+        {menuItems.map(({ label, path, icon }) => (
+          <NavLink key={path} to={path}>
+            <S.MenuItem>
+              {icon && <S.IconWrapper>{icon}</S.IconWrapper>}
+              {label}
+            </S.MenuItem>
+          </NavLink>
+        ))}
+      </S.MenuList>
+    </S.Container>
+  );
 };
 
 export default Sidebar;
