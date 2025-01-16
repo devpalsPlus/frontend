37817895170e2f5ad9d009768c54418eeb41337e
HongbiKo <rhghdql666@gmail.com>
2025-01-16 22:11:29 +0900

    feat: 마이페이지 생성

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,11 +1,33 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../../components/common/sidebar/Sidebar';
 import * as S from './MyPage.styled';
+import {
+  UserIcon,
+  DocumentTextIcon,
+  PencilSquareIcon,
+} from '@heroicons/react/24/outline';
 
 const MyPage = () => {
+  const menuItems = [
+    {
+      label: '프로필',
+      path: '/mypage',
+      icon: <UserIcon />,
+    },
+    {
+      label: '참여한 프로젝트 현황',
+      path: '/mypage/join-projects',
+      icon: <DocumentTextIcon />,
+    },
+    {
+      label: '지원한 프로젝트 현황',
+      path: '/mypage/apply-projects',
+      icon: <PencilSquareIcon />,
+    },
+  ];
   return (
     <S.Container>
-      <Sidebar />
+      <Sidebar menuItems={menuItems} nickname='백엔드에서 받아오는 이름' />
       <S.Wrapper>
         <Outlet />
       </S.Wrapper>
