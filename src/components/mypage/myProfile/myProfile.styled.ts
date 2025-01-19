82972999be7327d3c88d8c4dc8b8e940754ca737
HongbiKo <rhghdql666@gmail.com>
2025-01-19 20:15:18 +0900

    feat: 마이페이지 마이프로필 컴포넌트 생성 및 스타일링

--- src/components/mypage/myProfile/myProfile.styled.ts
+++ src/components/mypage/myProfile/myProfile.styled.ts
@@ -1,3 +1,257 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background-color: rgba(250, 250, 250, 0.8);
+  border-radius: ${({ theme }) => theme.borderRadius.large};
+  padding: 2.5rem 3rem;
+  position: relative;
+
+  form {
+    margin-top: 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+  }
+`;
+
+export const ProfileSection = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+
+  button {
+    position: absolute;
+    top: 3rem;
+    right: 3rem;
+    background-color: #3e5879;
+    padding: 0.5rem;
+    border-radius: 50%;
+
+    svg {
+      width: 1.3rem;
+      height: 1.3rem;
+      color: ${({ theme }) => theme.color.white};
+    }
+  }
+
+  a {
+    width: fit-content;
+    display: inline-block;
+    padding: 0.5rem 0.7rem;
+    border-radius: ${({ theme }) => theme.borderRadius.large};
+    background-color: #3e5879;
+    color: ${({ theme }) => theme.color.white};
+    font-size: 0.8rem;
+    margin-top: 1rem;
+  }
+`;
+
+export const IconWrapper = styled.div`
+  width: fit-content;
+  height: fit-content;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 0.5rem;
+  margin-top: -0.1rem;
+  background-color: ${({ theme }) => theme.color.white};
+  padding: 0.2rem;
+  border-radius: 50%;
+  border: 1px solid #f0f0f0;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1.25rem;
+
+  label {
+    margin-right: 1rem;
+    font-weight: 600;
+    color: ${({ theme }) => theme.color.deepGrey};
+  }
+
+  ul {
+    display: flex;
+    gap: 5px;
+
+    li {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      font-size: 0.7rem;
+      color: #a1a1a1;
+
+      img {
+        background-color: white;
+        border-radius: 50%;
+        border: 1px solid #f0f0f0;
+      }
+    }
+  }
+
+  button {
+    margin-right: 1rem;
+  }
+`;
+
+export const List = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1.25rem;
+
+  label {
+    font-weight: 600;
+    color: ${({ theme }) => theme.color.deepGrey};
+    margin-bottom: 0.6rem;
+  }
+  ul {
+    display: flex;
+    flex-direction: column;
+
+    li {
+      color: #a1a1a1;
+      margin-bottom: 0.8rem;
+
+      span {
+        color: ${({ theme }) => theme.color.primary};
+      }
+    }
+  }
+`;
+
+export const EditWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  align-items: center;
+  margin-bottom: 2.6rem;
+
+  label {
+    margin-right: 1rem;
+    font-weight: 600;
+    color: ${({ theme }) => theme.color.deepGrey};
+  }
+
+  button {
+    margin-left: 1rem;
+    padding: 0.65rem 1rem;
+    min-width: 60px;
+    font-size: 0.9rem;
+  }
+`;
+
+export const InputTextNickname = styled.div`
+  width: 20%;
+  min-width: 125px;
+`;
+
+export const InputTextGithub = styled.div`
+  width: 90%;
+`;
+
+export const InputTextCareer = styled.div`
+  width: 100%;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  width: 85%;
+`;
+
+export const ErrorMessage = styled.span<{ message?: string }>`
+  position: absolute;
+  bottom: -1.8rem;
+  left: 0.5rem;
+  display: inline-block;
+  color: #d43636;
+  font-size: 0.8rem;
+  height: 1.2rem;
+`;
+
+export const ErrorCareerMessage = styled.span<{ message?: string }>`
+  display: inline-block;
+  color: #d43636;
+  font-size: 0.7rem;
+`;
+
+export const EditContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  label {
+    margin-bottom: 1rem;
+    font-weight: 600;
+    color: ${({ theme }) => theme.color.deepGrey};
+  }
+
+  button {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    background-color: #3e5879;
+    svg {
+      color: ${({ theme }) => theme.color.white};
+    }
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+  }
+`;
+
+export const EditList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  width: 100%;
+  position: relative;
+  margin-bottom: 3rem;
+  background-color: ${({ theme }) => theme.color.white};
+  padding: 1rem;
+  padding-bottom: 0.3rem;
+  border-radius: 20px;
+
+  label {
+    margin-bottom: 0.7rem;
+  }
+
+  textarea {
+    width: 100%;
+    padding: 1rem;
+  }
+`;
+
+export const CareerList = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  gap: 1rem;
+  background-color: rgba(237, 237, 237, 0.73);
+  border-radius: 20px;
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+
+  button {
+    position: static;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 22px;
+    height: 22px;
+    border-radius: 50%;
+    top: 0;
+    right: 0.5rem;
+  }
+`;
+
+export const CareerWrapper = styled.div`
+  flex: 1;
+  position: relative;
+`;
--- src/components/mypage/myProfile/myProfile.tsx
+++ src/components/mypage/myProfile/myProfile.tsx
@@ -1,7 +1,495 @@
+import { z } from 'zod';
 import * as S from './myProfile.styled';
+import { ERROR_MESSAGES } from '../../../constants/authConstants';
+import { Controller, useFieldArray, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Title from '../../common/title/Title';
+import { useEffect, useState } from 'react';
+import { useSearchFilteringSkillTag } from '../../../hooks/useSearchFilteringSkillTag';
+import useNickNameVerification from '../../../hooks/useNicknameVerification';
+import InputText from '../../auth/InputText';
+import Button from '../../common/Button/Button';
+import {
+  useEditMyProfileInfo,
+  useMyProfileInfo,
+} from '../../../hooks/useMyProfileInfo';
+import {
+  PencilIcon,
+  XMarkIcon,
+  SquaresPlusIcon,
+} from '@heroicons/react/24/outline';
+import { ROUTES } from '../../../constants/routes';
+import { Link } from 'react-router-dom';
+import BeginnerIcon from '../../../assets/beginner.svg';
+import OptionBox from './optionBox';
+
+const profileSchema = z.object({
+  nickname: z
+    .string()
+    .nonempty(ERROR_MESSAGES.NICKNAME_REQUIRED)
+    .max(6, ERROR_MESSAGES.NICKNAME_LENGTH)
+    .regex(
+      /^[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9~`!@#$%^&*()\-_=+]{1,6}$/,
+      ERROR_MESSAGES.NICKNAME_FORMAT
+    ),
+  skillTagIds: z.array(z.number()).min(1, ERROR_MESSAGES.SKILL_REQUIRED),
+  positionTagId: z.number().refine((id) => id > 0, {
+    message: ERROR_MESSAGES.POSITION_REQUIRED,
+  }),
+  github: z
+    .string()
+    .optional()
+    .optional()
+    .refine(
+      (val) => !val || /^https?:\/\/[^\s$.?#].[^\s]*$/.test(val),
+      ERROR_MESSAGES.GITHUB_SPECIAL
+    )
+    .transform((val) => (val === '' ? '' : val || '')),
+  career: z
+    .array(
+      z
+        .object({
+          name: z.string().nonempty(ERROR_MESSAGES.CAREERNAME_REQUIRED),
+          periodStart: z.string().nonempty(ERROR_MESSAGES.STARTPERIOD_REQUIRED),
+          periodEnd: z.string().nonempty(ERROR_MESSAGES.ENDPERIOD_REQUIRED),
+          role: z.string().nonempty(ERROR_MESSAGES.ROLE_REQUIRED),
+        })
+        .refine(
+          (data) => new Date(data.periodStart) <= new Date(data.periodEnd),
+          {
+            message: ERROR_MESSAGES.ENDPERIOD_SPECIAL,
+            path: ['periodEnd'],
+          }
+        )
+    )
+    .optional(),
+  bio: z.string().optional(),
+});
+
+type ProfileFormData = z.infer<typeof profileSchema>;
 
 const MyProfile = () => {
-  return <S.Container>마이 프로필</S.Container>;
+  const [isEditing, setIsEditing] = useState(false);
+  const { skillTagsData, positionTagsData } = useSearchFilteringSkillTag();
+  const { nicknameMessage, handleNickNameChange, handleNickname } =
+    useNickNameVerification();
+
+  const { myData, isLoading } = useMyProfileInfo();
+  const { editMyProfile } = useEditMyProfileInfo();
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<ProfileFormData>({
+    resolver: zodResolver(profileSchema),
+    defaultValues: {
+      nickname: '',
+      skillTagIds: [],
+      positionTagId: 0,
+      github: '',
+      career: [],
+      bio: '',
+    },
+    mode: 'onChange',
+  });
+
+  useEffect(() => {
+    if (myData) {
+      const skillTagIds = myData.skills
+        .map(
+          (skill) =>
+            skillTagsData.find((tag) => tag.name === skill.skillName)?.id
+        )
+        .filter((id): id is number => id !== undefined);
+
+      reset({
+        nickname: myData.nickname,
+        bio: myData.bio || '',
+        positionTagId: myData.positionTag?.id || 0,
+        skillTagIds: skillTagIds,
+        career: myData.career?.length
+          ? myData.career
+          : [{ name: '', periodStart: '', periodEnd: '', role: '' }],
+      });
+    }
+  }, [myData, skillTagsData, reset]);
+
+  const { fields, append, remove } = useFieldArray({ control, name: 'career' });
+
+  const handleCheckNickName = (nickname: string) => {
+    if (nickname !== myData?.nickname) {
+      handleNickname(nickname);
+    }
+  };
+
+  const onSubmit = (data: ProfileFormData, e?: React.BaseSyntheticEvent) => {
+    e?.preventDefault();
+
+    editMyProfile(data);
+    setIsEditing(false);
+  };
+
+  if (isLoading) {
+    return <div>로딩중...</div>;
+  }
+  if (!myData) {
+    return <div>유저 정보를 불러 올 수 없습니다.</div>;
+  }
+
+  return (
+    <S.Container>
+      <Title size='semiLarge'>나의 정보</Title>
+      {!isEditing ? (
+        <S.ProfileSection>
+          <S.Wrapper>
+            <label>닉네임</label>
+            <span>{myData.nickname}</span>
+            <S.IconWrapper>
+              {myData.userLevel === 'Beginner' ? (
+                <img src={BeginnerIcon} alt='beginner' width='16' height='16' />
+              ) : (
+                ''
+              )}
+            </S.IconWrapper>
+          </S.Wrapper>
+          <S.Wrapper>
+            <label>스킬셋</label>
+            <ul>
+              {myData.skills.map((skill) => (
+                <li key={skill.skillName}>
+                  <img
+                    src={skill.skillImg}
+                    alt={skill.skillName}
+                    width='40'
+                    height='40'
+                  />
+                  <span>{skill.skillName}</span>
+                </li>
+              ))}
+            </ul>
+          </S.Wrapper>
+          <S.Wrapper>
+            <label>포지션</label>
+            <span>{myData.positionTag?.name}</span>
+          </S.Wrapper>
+          <S.Wrapper>
+            <label>깃허브</label>
+            <span>{myData.github}</span>
+          </S.Wrapper>
+          <S.List>
+            <label>경&nbsp;&nbsp;&nbsp;력</label>
+            <ul>
+              {myData.career?.map((career) => (
+                <li key={career.name}>
+                  <span>{career.name}</span> ({career.periodStart.slice(0, 10)}{' '}
+                  ~ {career.periodEnd.slice(0, 10)}{' '}
+                  <span> - {career.role}</span>)
+                </li>
+              ))}
+            </ul>
+          </S.List>
+          <S.List>
+            <label>소&nbsp;&nbsp;&nbsp;개</label>
+            <p>{myData.bio}</p>
+          </S.List>
+          <Link to={ROUTES.changePw}>비밀번호 재설정</Link>
+          <button onClick={() => setIsEditing(true)}>
+            <PencilIcon />
+          </button>
+        </S.ProfileSection>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* 닉네임 */}
+          <S.EditWrapper>
+            <label>닉네임</label>
+            <Controller
+              name='nickname'
+              control={control}
+              render={({ field }) => (
+                <S.InputWrapper>
+                  <S.InputTextNickname>
+                    <InputText
+                      inputType='text'
+                      placeholder='닉네임'
+                      autoComplete='off'
+                      {...field}
+                      onChange={(e) => {
+                        const value = e.target.value;
+                        field.onChange(e);
+                        handleNickNameChange(value);
+                      }}
+                    />
+                  </S.InputTextNickname>
+                  {errors.nickname && (
+                    <S.ErrorMessage>{errors.nickname.message}</S.ErrorMessage>
+                  )}
+                  {!errors.nickname && nicknameMessage && (
+                    <S.ErrorMessage>{nicknameMessage}</S.ErrorMessage>
+                  )}
+                  <Button
+                    size='primary'
+                    schema='primary'
+                    radius='large'
+                    type='button'
+                    onClick={() => {
+                      handleCheckNickName(field.value);
+                    }}
+                  >
+                    중복확인
+                  </Button>
+                </S.InputWrapper>
+              )}
+            />
+          </S.EditWrapper>
+
+          {/* 스킬셋 */}
+          <S.EditContainer>
+            <label>스킬셋</label>
+            <Controller
+              name='skillTagIds'
+              control={control}
+              render={({ field }) => (
+                <S.EditList>
+                  {skillTagsData.map((skill) => (
+                    <OptionBox
+                      key={skill.id}
+                      id={skill.id}
+                      label={skill.name}
+                      type='checkbox'
+                      isSelected={field.value.includes(skill.id)}
+                      onChange={(id, isChecked) => {
+                        if (isChecked) {
+                          field.onChange([...field.value, id]);
+                        } else {
+                          field.onChange(
+                            field.value.filter((value) => value !== id)
+                          );
+                        }
+                      }}
+                      hasImage={true}
+                      imgSrc={skill.img}
+                    />
+                  ))}
+                  {errors.skillTagIds && (
+                    <S.ErrorMessage>
+                      {errors.skillTagIds.message}
+                    </S.ErrorMessage>
+                  )}
+                </S.EditList>
+              )}
+            />
+          </S.EditContainer>
+
+          {/* 포지션 */}
+          <S.EditContainer>
+            <label>포지션</label>
+            <Controller
+              name='positionTagId'
+              control={control}
+              render={({ field }) => (
+                <S.EditList>
+                  {positionTagsData
+                    .filter((position) => position.name !== '전체')
+                    .map((position) => (
+                      <OptionBox
+                        key={position.id}
+                        id={position.id}
+                        label={position.name}
+                        type='radio'
+                        isSelected={field.value === position.id}
+                        onChange={(id) => field.onChange(id)}
+                        hasImage={false}
+                      />
+                    ))}
+                  {errors.positionTagId && (
+                    <S.ErrorMessage>
+                      {errors.positionTagId.message}
+                    </S.ErrorMessage>
+                  )}
+                </S.EditList>
+              )}
+            />
+          </S.EditContainer>
+
+          {/* 깃허브 */}
+          <S.EditWrapper>
+            <label>깃허브</label>
+            <Controller
+              name='github'
+              control={control}
+              render={({ field }) => (
+                <S.InputWrapper>
+                  <S.InputTextGithub>
+                    <InputText
+                      inputType='text'
+                      placeholder='깃허브 주소를 입력해주세요.'
+                      autoComplete='off'
+                      {...field}
+                    />
+                  </S.InputTextGithub>
+                  {errors.github && (
+                    <S.ErrorMessage>{errors.github.message}</S.ErrorMessage>
+                  )}
+                </S.InputWrapper>
+              )}
+            />
+          </S.EditWrapper>
+
+          {/* 경력 */}
+          <S.EditContainer>
+            <label>경&nbsp;&nbsp;&nbsp;력</label>
+            <S.EditList>
+              {fields.map((field, index) => (
+                <S.CareerList key={field.id}>
+                  <Controller
+                    name={`career.${index}.name`}
+                    control={control}
+                    render={({ field }) => (
+                      <S.CareerWrapper>
+                        <S.InputTextCareer>
+                          <InputText
+                            inputType='text'
+                            placeholder='회사 이름'
+                            autoComplete='off'
+                            {...field}
+                          />
+                        </S.InputTextCareer>
+                        {errors.career?.[index]?.name && (
+                          <S.ErrorCareerMessage>
+                            {errors.career[index]?.name?.message}
+                          </S.ErrorCareerMessage>
+                        )}
+                      </S.CareerWrapper>
+                    )}
+                  />
+                  <Controller
+                    name={`career.${index}.periodStart`}
+                    control={control}
+                    render={({ field }) => (
+                      <S.CareerWrapper>
+                        <S.InputTextCareer>
+                          <InputText
+                            inputType='date'
+                            placeholder='시작 날짜 (YYYY-MM-DD)'
+                            autoComplete='off'
+                            {...field}
+                          />
+                        </S.InputTextCareer>
+                        {errors.career?.[index]?.periodStart && (
+                          <S.ErrorCareerMessage>
+                            {errors.career[index]?.periodStart?.message}
+                          </S.ErrorCareerMessage>
+                        )}
+                      </S.CareerWrapper>
+                    )}
+                  />
+                  <Controller
+                    name={`career.${index}.periodEnd`}
+                    control={control}
+                    render={({ field }) => (
+                      <S.CareerWrapper>
+                        <S.InputTextCareer>
+                          <InputText
+                            inputType='date'
+                            placeholder='종료 날짜  (YYYY-MM-DD)'
+                            autoComplete='off'
+                            {...field}
+                          />
+                        </S.InputTextCareer>
+                        {errors.career?.[index]?.periodEnd && (
+                          <S.ErrorCareerMessage>
+                            {errors.career[index]?.periodEnd?.message}
+                          </S.ErrorCareerMessage>
+                        )}
+                      </S.CareerWrapper>
+                    )}
+                  />
+                  <Controller
+                    name={`career.${index}.role`}
+                    control={control}
+                    render={({ field }) => (
+                      <S.CareerWrapper>
+                        <S.CareerWrapper>
+                          <S.InputTextCareer>
+                            <InputText
+                              inputType='text'
+                              placeholder='역할'
+                              autoComplete='off'
+                              {...field}
+                            />
+                          </S.InputTextCareer>
+                        </S.CareerWrapper>
+                        {errors.career?.[index]?.role && (
+                          <S.ErrorCareerMessage>
+                            {errors.career[index]?.role?.message}
+                          </S.ErrorCareerMessage>
+                        )}
+                      </S.CareerWrapper>
+                    )}
+                  />
+                  <button onClick={() => remove(index)}>
+                    <XMarkIcon width='16' height='16' />
+                  </button>
+                </S.CareerList>
+              ))}
+            </S.EditList>
+            <button
+              type='button'
+              onClick={() =>
+                append({ name: '', periodStart: '', periodEnd: '', role: '' })
+              }
+            >
+              <SquaresPlusIcon width='20' height='20' />
+            </button>
+            {errors.career && (
+              <S.ErrorMessage>{errors.career.message}</S.ErrorMessage>
+            )}
+          </S.EditContainer>
+
+          {/* 소개 */}
+          <S.EditContainer>
+            <label>소&nbsp;&nbsp;&nbsp;개</label>
+            <S.EditList>
+              <Controller
+                name='bio'
+                control={control}
+                render={({ field }) => (
+                  <textarea
+                    {...field}
+                    placeholder='자신의 소개를 입력해주세요.'
+                  />
+                )}
+              />
+              {errors.bio && (
+                <S.ErrorMessage>{errors.bio.message}</S.ErrorMessage>
+              )}
+            </S.EditList>
+          </S.EditContainer>
+          <S.Wrapper>
+            <Button
+              size='primary'
+              schema='primary'
+              radius='large'
+              type='submit'
+            >
+              확인
+            </Button>
+            <Button
+              size='primary'
+              schema='primary'
+              radius='large'
+              type='button'
+              onClick={() => setIsEditing(false)}
+            >
+              취소
+            </Button>
+          </S.Wrapper>
+        </form>
+      )}
+    </S.Container>
+  );
 };
 
 export default MyProfile;
