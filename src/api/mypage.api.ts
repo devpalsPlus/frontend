e7377195f79df97a124f57cd7ec4ca88dc291a15
HongbiKo <rhghdql666@gmail.com>
2025-01-19 20:14:48 +0900

    fat: 마이페이지 조회 및 수정 api 및 커스텀훅 추가

---
+++ src/api/mypage.api.ts
@@ -0,0 +1,24 @@
+import { EditMyInfo, MyInfo } from '../models/myInfo';
+import { httpClient } from './http.api';
+
+export const getMyInfo = async () => {
+  try {
+    const response = await httpClient.get<MyInfo>('/user/me');
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error('mypage-myinfo:', error);
+    throw error;
+  }
+};
+
+export const putMyInfo = async (data: EditMyInfo) => {
+  try {
+    const response = await httpClient.put('/user/me', data);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error('mypage-myinfoedit:', error);
+    throw error;
+  }
+};
---
+++ src/hooks/useMyProfileInfo.ts
@@ -0,0 +1,42 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { getMyInfo, putMyInfo } from '../api/mypage.api';
+import { EditMyInfo, MyInfo } from '../models/myInfo';
+import { useAlert } from './useAlert';
+import { AxiosError } from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+export const useMyProfileInfo = () => {
+  const { data, isLoading } = useQuery<MyInfo>({
+    queryKey: ['myInfo'],
+    queryFn: () => getMyInfo(),
+    staleTime: 1 * 60 * 1000,
+  });
+
+  return { myData: data, isLoading };
+};
+
+export const useEditMyProfileInfo = () => {
+  const navigate = useNavigate();
+  const { showAlert } = useAlert();
+  const queryClient = useQueryClient();
+
+  const editProfileMutation = useMutation<void, AxiosError, EditMyInfo>({
+    mutationFn: async (data: EditMyInfo) => {
+      await putMyInfo(data);
+    },
+    onSuccess: () => {
+      showAlert('프로필 수정이 완료되었습니다.');
+      navigate('/mypage');
+      queryClient.invalidateQueries({ queryKey: ['myInfo'] });
+    },
+    onError: () => {
+      showAlert('프로필 수정에 실패했습니다.');
+    },
+  });
+
+  const editMyProfile = (data: EditMyInfo) => {
+    editProfileMutation.mutate(data);
+  };
+
+  return { editMyProfile };
+};
