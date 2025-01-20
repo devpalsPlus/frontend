292ee9eb10b8e6f3abb0726e7a0426d61af261c5
HongbiKo <rhghdql666@gmail.com>
2025-01-20 14:54:04 +0900

    feat: 라우터 상수 변경 및 추가

--- src/hooks/queries/keys.ts
+++ src/hooks/queries/keys.ts
@@ -2,12 +2,14 @@ export const managedProjectsKey = {
   mine: ['myManagedProjects'],
 } as const;
 
-
 export const applicantKey = {
   info: ['applicantInfo'],
 };
 
 export const myInfoKey = {
-  myProfile: ['myInfo'],
+  myProfile: ['myProfile'],
 } as const;
 
+export const userInfoKey = {
+  userProfile: ['userProfile'],
+} as const;
