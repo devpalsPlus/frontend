8b3ab5f826cc05192874cf4f04631e7d4e9cfc8b
HongbiKo <rhghdql666@gmail.com>
2025-01-20 22:51:56 +0900

    fix: 로그인시에만 myinfo, userinfo api 호출

--- src/hooks/useMyInfo.ts
+++ src/hooks/useMyInfo.ts
@@ -1,17 +1,21 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { getMyInfo, putMyInfo } from '../api/mypage.api';
+import { getMyInfo, patchMyProfileImg, putMyInfo } from '../api/mypage.api';
 import { EditMyInfo, UserInfo } from '../models/userInfo';
 import { useAlert } from './useAlert';
 import { AxiosError } from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '../constants/routes';
 import { myInfoKey } from './queries/keys';
+import useAuthStore from '../store/authStore';
 
 export const useMyProfileInfo = () => {
+  const { isLoggedIn } = useAuthStore();
+
   const { data, isLoading } = useQuery<UserInfo>({
     queryKey: myInfoKey.myProfile,
     queryFn: () => getMyInfo(),
     staleTime: 1 * 60 * 1000,
+    enabled: !!isLoggedIn,
   });
 
   return { myData: data, isLoading };
@@ -41,4 +45,28 @@ export const useEditMyProfileInfo = () => {
   };
 
   return { editMyProfile };
+};
+
+export const useUploadProfileImg = () => {
+  const queryClient = useQueryClient();
+  const { showAlert } = useAlert();
+
+  const uploadProfileImgMutation = useMutation<void, AxiosError, File>({
+    mutationFn: async (data: File) => {
+      await patchMyProfileImg(data);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: myInfoKey.myProfile });
+      showAlert('프로필 이미지가 업로드 되었습니다.');
+    },
+    onError: () => {
+      showAlert('이미지는 1MB 이하, .PNG.JPG 형식만 가능합니다.');
+    },
+  });
+
+  const uploadProfileImg = (data: File) => {
+    uploadProfileImgMutation.mutate(data);
+  };
+
+  return { uploadProfileImg };
 };
--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -2,12 +2,16 @@ import { useQuery } from '@tanstack/react-query';
 import { UserInfo } from '../models/userInfo';
 import { userInfoKey } from './queries/keys';
 import { getUserInfo } from '../api/userpage.api';
+import useAuthStore from '../store/authStore';
 
 export const useUserProfileInfo = (id: number) => {
+  const { isLoggedIn } = useAuthStore();
+
   const { data, isLoading } = useQuery<UserInfo>({
     queryKey: [userInfoKey.userProfile, id],
     queryFn: () => getUserInfo(id),
     staleTime: 1 * 60 * 1000,
+    enabled: !!isLoggedIn,
   });
 
   return { userData: data, isLoading };
