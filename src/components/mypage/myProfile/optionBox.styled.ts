6ab000dfa552d4f512757752978ca0e8d6f48523
HongbiKo <rhghdql666@gmail.com>
2025-01-20 12:34:53 +0900

    fix: optionBox 스타일 props 설정

--- src/components/mypage/myProfile/optionBox.styled.ts
+++ src/components/mypage/myProfile/optionBox.styled.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const OptionLabel = styled.label<{ isSelected: boolean }>`
+export const OptionLabel = styled.label<{ $isSelected: boolean }>`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -8,6 +8,7 @@ export const OptionLabel = styled.label<{ isSelected: boolean }>`
   gap: 0.2rem;
   cursor: pointer;
   transition: border-color 0.3s;
+  margin-bottom: 0;
 
   &:hover img {
     border-color: #3e5879;
@@ -21,13 +22,13 @@ export const OptionLabel = styled.label<{ isSelected: boolean }>`
     border: 1px solid #f0f0f0;
     border-radius: 50%;
     transition: border-color 0.3s;
-    border-color: ${({ isSelected }) => (isSelected ? '#3e5879' : '#f0f0f0')};
+    border-color: ${({ $isSelected }) => ($isSelected ? '#3e5879' : '#f0f0f0')};
   }
 
   span {
     font-size: 0.8rem;
     font-weight: 400;
-    color: ${({ isSelected }) => (isSelected ? '#3e5879' : '#a1a1a1')};
+    color: ${({ $isSelected }) => ($isSelected ? '#3e5879' : '#a1a1a1')};
     transition: border-color 0.3s;
   }
 `;
--- src/components/mypage/myProfile/optionBox.tsx
+++ src/components/mypage/myProfile/optionBox.tsx
@@ -7,7 +7,6 @@ interface OptionBoxProps {
   type: 'checkbox' | 'radio';
   isSelected: boolean;
   onChange: (id: number, isChecked?: boolean) => void;
-  hasImage?: boolean;
   imgSrc?: string;
 }
 
@@ -17,7 +16,6 @@ const OptionBox = ({
   type,
   isSelected,
   onChange,
-  hasImage = false,
   imgSrc,
 }: OptionBoxProps) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,16 +24,14 @@ const OptionBox = ({
   };
 
   return (
-    <S.OptionLabel isSelected={isSelected}>
+    <S.OptionLabel $isSelected={isSelected}>
       <S.HiddenInput
         type={type}
         value={id}
         checked={isSelected}
         onChange={handleChange}
       />
-      {hasImage && imgSrc && (
-        <img src={imgSrc} alt={label} width='40' height='40' />
-      )}
+      {imgSrc && <img src={imgSrc} alt={label} width='40' height='40' />}
       <span>{label}</span>
     </S.OptionLabel>
   );
