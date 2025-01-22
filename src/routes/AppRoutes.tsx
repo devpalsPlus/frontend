44a2e18ff4c3c0354187b41af59ad5a6e09bcf80
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-01-22 16:15:47 +0900

    feat : 페이지 접근 시 로그인 여부 확인

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -96,33 +96,41 @@ const AppRoutes = () => {
     {
       path: ROUTES.createProject,
       element: (
-        <Layout>
-          <CreateProject />
-        </Layout>
+        <ProtectRoute redirectUrl={ROUTES.login}>
+          <Layout>
+            <CreateProject />
+          </Layout>
+        </ProtectRoute>
       ),
     },
     {
       path: `${ROUTES.apply}/:projectId`,
       element: (
-        <Layout>
-          <Apply />
-        </Layout>
+        <ProtectRoute redirectUrl={ROUTES.login}>
+          <Layout>
+            <Apply />
+          </Layout>
+        </ProtectRoute>
       ),
     },
     {
       path: `${ROUTES.projectDetail}/:projectId`,
       element: (
-        <Layout>
-          <ProjectDetail />
-        </Layout>
+        <ProtectRoute redirectUrl={ROUTES.login}>
+          <Layout>
+            <ProjectDetail />
+          </Layout>
+        </ProtectRoute>
       ),
     },
     {
       path: `${ROUTES.modifyProject}/:projectId`,
       element: (
-        <Layout>
-          <ModifyProject />
-        </Layout>
+        <ProtectRoute redirectUrl={ROUTES.login}>
+          <Layout>
+            <ModifyProject />
+          </Layout>
+        </ProtectRoute>
       ),
     },
     {
