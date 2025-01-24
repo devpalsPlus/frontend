8af0505f0831bea643489e37d0fa565a3b4a31f3
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-01-24 23:49:09 +0900

    feat: modal message 상수화

---
+++ src/constants/modalMessage.ts
@@ -0,0 +1,7 @@
+export const MODAL_MESSAGE = {
+  pass: '지원자를 합격 리스트에 추가했습니다.',
+  nonPass: '지원자를 불합격 리스트에 추가했습니다.',
+  equalStatus: '이미 동일한 리스트에 추가하신 상태입니다.',
+  signUp: '회원가입에 성공하셨습니다!',
+  login: '로그인 되었습니다.',
+} as const;
