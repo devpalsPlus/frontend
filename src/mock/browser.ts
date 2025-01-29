010b09acd58aa24b79011b1dc9de6c2a2b2da2cc
HongbiKo <rhghdql666@gmail.com>
2025-01-30 01:07:59 +0900

    feat: 목서버 데이터 추가 및 설정

--- src/mock/browser.ts
+++ src/mock/browser.ts
@@ -9,12 +9,14 @@ import {
 import { projectDetail } from './projectDetail';
 import {
   myPageAppliedProjectList,
+  mypageEditProfile,
   myPageJoinedProjectList,
   myPagePositionTag,
   myPageProfile,
   myPageSkillTag,
 } from './mypage';
 import { userPageAppliedProjectList, userPageProfile } from './userpage';
+import { login } from './auth';
 
 export const handlers = [
   myProjectList,
@@ -30,6 +32,8 @@ export const handlers = [
   userPageProfile,
   userPageAppliedProjectList,
   passNonPassList,
+  mypageEditProfile,
+  login,
 ];
 
 export const worker = setupWorker(...handlers);
