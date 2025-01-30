7ea2573d67aac0885e9ca06e07a5f44e90192cfc
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-01-30 18:01:46 +0900

    refactor: props isPass -> status로 변경 및 쿼리무효화 추가

--- src/components/manageProjects/passNonPassList/PassNonPassList.tsx
+++ src/components/manageProjects/passNonPassList/PassNonPassList.tsx
@@ -1,16 +1,21 @@
+import { useMutationParams } from '../../../hooks/usePassNonPassMutation';
 import { ApplicantInfo } from '../../../models/applicant';
 import PassNonPassItem from './PassNonPassItem';
 import * as S from './PassNonPassList.styled';
 
 interface PassNonPassListProps {
   passNonPassListData: ApplicantInfo[];
+  onClick: ({ status, userId }: useMutationParams) => void;
 }
 
-function PassNonPassList({ passNonPassListData }: PassNonPassListProps) {
+function PassNonPassList({
+  passNonPassListData,
+  onClick,
+}: PassNonPassListProps) {
   return (
     <S.Wrapper>
       {passNonPassListData.map((data) => (
-        <PassNonPassItem key={data.userId} userInfo={data} />
+        <PassNonPassItem key={data.userId} onClick={onClick} userInfo={data} />
       ))}
     </S.Wrapper>
   );
