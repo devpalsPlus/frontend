fe03ecef69a4e2e18fad6b9de34fea4e9642b3c2
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-01-30 18:02:37 +0900

    feat: modal 메세지 추가

--- src/constants/modalMessage.ts
+++ src/constants/modalMessage.ts
@@ -1,7 +1,10 @@
 export const MODAL_MESSAGE = {
   pass: '지원자를 합격 리스트에 추가했습니다.',
   nonPass: '지원자를 불합격 리스트에 추가했습니다.',
+  waiting: '지원자를 대기리스트에 추가했습니다',
   equalStatus: '이미 동일한 리스트에 추가하신 상태입니다.',
   signUp: '회원가입에 성공하셨습니다!',
   login: '로그인 되었습니다.',
+  sendResult: '지원자들에게 이메일로 결과를 전송했어요!',
+  needAuth: '권한이 없어요!',
 } as const;
