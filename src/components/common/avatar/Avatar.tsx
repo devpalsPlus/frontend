ab334d6ba6e8971a49653e76ba960f38235d8c3e
HongbiKo <rhghdql666@gmail.com>
2025-01-31 22:51:00 +0900

    fix: 사이드바 로고이미지 적용시에 border, radius 삭제

--- src/components/common/avatar/Avatar.tsx
+++ src/components/common/avatar/Avatar.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import * as S from './Avatar.styled';
 import defaultImg from '../../../assets/defaultImg.png';
+import MainLogo from '../../../assets/mainlogo.svg';
 
 export interface AvatarProps {
   size: string;
@@ -8,10 +9,11 @@ export interface AvatarProps {
 }
 
 function Avatar({ size, image }: AvatarProps) {
+  const isMainLogo = image === MainLogo;
   const releasedImg =
     typeof image === 'string' && image.trim() ? image : defaultImg;
   return (
-    <S.AvatarContainer size={size}>
+    <S.AvatarContainer size={size} $isMainLogo={isMainLogo}>
       {typeof image === 'string' || !image ? (
         <img className='avatar' src={releasedImg} alt='Avatar' />
       ) : (
