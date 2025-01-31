727d430e57ad33dab1a05a1553c819ffbc40a5b1
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-01-31 11:52:32 +0900

    feat: disabled 속성 추가

--- src/components/manageProjects/passNonPassList/PassNonPassItem.tsx
+++ src/components/manageProjects/passNonPassList/PassNonPassItem.tsx
@@ -1,18 +1,25 @@
 import { useMutationParams } from '../../../hooks/usePassNonPassMutation';
 import { ApplicantInfo } from '../../../models/applicant';
+import { ProjectDetailExtended } from '../../../models/projectDetail';
 import DeleteButton from './DeleteButton';
 import * as S from './PassNonPassItem.styled';
 
 interface PassNonPassItemProps {
   userInfo: ApplicantInfo;
+  projectData: ProjectDetailExtended;
   onClick: ({ status, userId }: useMutationParams) => void;
 }
 
-function PassNonPassItem({ userInfo, onClick }: PassNonPassItemProps) {
+function PassNonPassItem({
+  userInfo,
+  onClick,
+  projectData,
+}: PassNonPassItemProps) {
   return (
     <S.ItemWrapper>
       <S.NickName>{userInfo.User.nickname}</S.NickName>
       <DeleteButton
+        disabled={projectData.isDone}
         onClick={() => onClick({ status: 'WAITING', userId: userInfo.userId })}
       />
     </S.ItemWrapper>
