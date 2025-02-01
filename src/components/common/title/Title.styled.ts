358ba7905a5408493fe3c280392ba625a1665b0f
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-02-01 16:19:09 +0900

    feat: elipsis 박스 추가

--- src/components/common/title/Title.styled.ts
+++ src/components/common/title/Title.styled.ts
@@ -4,6 +4,11 @@ import { TitleProps } from './Title';
 export const Container = styled.h1<Omit<TitleProps, 'children'>>`
   font-size: ${({ theme, size }) => theme.heading[size].fontSize};
   color: ${({ theme }) => theme.color.primary};
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
+  overflow: hidden;
 
   @media screen and ${({ theme }) => theme.mediaQuery.tablet} {
     font-size: ${({ theme, size }) => theme.heading[size].tabletFontSize};
