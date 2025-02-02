7779d619c0fc7f2c8dea31514933f739887b0d24
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-02-02 14:01:43 +0900

    refactor: 모달 취소 버튼 추출

--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -1,8 +1,9 @@
 import { useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
-import { XMarkIcon, CheckCircleIcon } from '@heroicons/react/24/outline';
+import { CheckCircleIcon } from '@heroicons/react/24/outline';
 import * as S from './Modal.styled';
 import ScrollPreventor from './ScrollPreventor';
+import ModalCloseButton from './ModalCloseButton';
 interface ModalProps {
   children: React.ReactNode;
   isOpen: boolean;
@@ -40,9 +41,7 @@ const Modal = ({ children, isOpen, onClose }: ModalProps) => {
         onAnimationEnd={handleAnimationEnd}
       >
         <S.ModalBody ref={modalRef}>
-          <S.ModalCloseButton onClick={handleClose}>
-            <XMarkIcon />
-          </S.ModalCloseButton>
+          <ModalCloseButton onClose={handleClose} />
           <S.ModalIconWrapper>
             <CheckCircleIcon />
           </S.ModalIconWrapper>
