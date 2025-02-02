c84771d7f3ff188ad6564995df83ffca763c840c
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-02-02 20:59:27 +0900

    feat: 텍스트 압축 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import type { UserConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      threshold: 2048,
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
