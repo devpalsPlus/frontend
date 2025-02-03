7f80056640afa3f2da8726af4f814c08154c0f4b
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-02-03 09:52:17 +0900

    refactor: className -> 스타일 컴포넌트 변경

--- src/components/common/dropDown/DropDown.styled.ts
+++ src/components/common/dropDown/DropDown.styled.ts
@@ -2,19 +2,21 @@ import styled from 'styled-components';
 
 export const DropDownContainer = styled.div`
   position: relative;
-  .toggle {
-    background: none;
-    border: none;
-    cursor: pointer;
-    outline: none;
-  }
+`;
 
-  .panel {
-    position: absolute;
-    top: 50px;
-    right: 0;
-    background-color: ${({ theme }) => theme.color.white};
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    z-index: 100;
-  }
+export const DropDownButtonWrapper = styled.div`
+  background: none;
+  border: none;
+  cursor: pointer;
+  outline: none;
+`;
+
+export const Panel = styled.div`
+  position: absolute;
+  top: 50px;
+  right: 0;
+  background-color: ${({ theme }) => theme.color.white};
+  border-radius: ${({ theme }) => theme.borderRadius.primary};
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  z-index: 100;
 `;
