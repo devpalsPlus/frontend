ad476c5061a26c800e068000ea7347fdeba8f4c8
HongbiKo <rhghdql666@gmail.com>
2025-02-06 12:27:51 +0900

    fix: accessibility 오류 수정

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,9 +1,18 @@
 import type { TestRunnerConfig } from '@storybook/test-runner';
 import { injectAxe, checkA11y } from 'axe-playwright';
 
+declare global {
+  interface Window {
+    axeRunning?: boolean;
+  }
+}
+
 const config: TestRunnerConfig = {
   async preVisit(page) {
     await injectAxe(page);
+    await page.evaluate(() => {
+      window.axeRunning = false;
+    });
   },
 
   async postVisit(page) {
