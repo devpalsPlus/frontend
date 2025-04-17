b4a3d14e2e164a86129e7051fb2c6df735db2cdb
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-04-17 23:44:10 +0900

    feat : 답글 컴포넌트 구현

---
+++ src/components/command/replyComponent/ReplyComponent.styled.ts
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 20px;
+  margin-left: 110px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-grow: 1;
+`;
+
+export const CommandWrapper = styled.div`
+  margin-top: 4px;
+  flex: 1;
+`;
+
+export const NickName = styled.p`
+  font-size: ${({ theme }) => theme.heading.xsSmall.fontSize};
+  margin-left: 11px;
+  margin-bottom: 3px;
+`;
+
+export const Command = styled.span`
+  margin-left: 11px;
+`;
+
+export const ReplyContainer = styled.div`
+  margin-top: 11px;
+  margin-left: 11px;
+`;
+
+export const ReplyButton = styled.div`
+  display: flex;
+  margin-top: 11px;
+`;
+
+export const Icon = styled.div`
+  margin-left: 11px;
+`;
+
+export const ReplyContent = styled.p`
+  margin-left: 10px;
+  margin-bottom: 10px;
+`;
+
+export const CommandInput = styled.div`
+  text-indent: 20px;
+`;
+
+export const ReplyInput = styled.div`
+  width: 100%;
+  padding-left: 15px;
+  margin-bottom: 10px;
+`;
+
+export const ErrorMessage = styled.div`
+  padding-left: 15px;
+  margin-bottom: 10px;
+`;
+
+export const Message = styled.p`
+  color: ${({ theme }) => theme.color.red};
+  font-size: 10px;
+`;
