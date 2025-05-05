46b5ab9dabd876ba2f421d8c4b591482d1c2097b
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2025-05-05 21:05:41 +0900

    feat: 로그인 권한 있는 곳에서 logout시 뜨는 alert modal로 변경

--- src/components/common/ProtectRoute.tsx
+++ src/components/common/ProtectRoute.tsx
@@ -1,16 +1,38 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
 import useAuthStore from '../../store/authStore';
 import { Navigate } from 'react-router-dom';
+import { useModal } from '../../hooks/useModal';
+import Modal from './modal/Modal';
+import { MODAL_MESSAGE } from '../../constants/modalMessage';
 interface ProtectRouteProps extends PropsWithChildren {
   redirectUrl: string;
 }
 
 const ProtectRoute = ({ children, redirectUrl }: ProtectRouteProps) => {
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+  const { isOpen, message, handleModalOpen, handleModalClose } = useModal();
+  const [shouldRedirect, setShouldRedirect] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      handleModalOpen(MODAL_MESSAGE.isNotLoggedIn);
+      const timer = setTimeout(() => setShouldRedirect(true), 1000);
+      return () => clearTimeout(timer);
+    } else {
+      setShouldRedirect(false);
+    }
+  }, [isLoggedIn, handleModalOpen]);
 
   if (!isLoggedIn) {
-    alert('로그인이 필요한 서비스입니다.');
-    return <Navigate to={redirectUrl} replace />;
+    if (shouldRedirect) {
+      return <Navigate to={redirectUrl} replace />;
+    }
+
+    return (
+      <Modal isOpen={isOpen} onClose={handleModalClose}>
+        {message}
+      </Modal>
+    );
   }
 
   return <>{children}</>;
