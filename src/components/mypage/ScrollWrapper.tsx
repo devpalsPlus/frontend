4f7b16c393f47bbdda7e1ee11da937c37b61bdeb
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2025-05-06 17:38:02 +0900

    fix: 필요없는 콘솔로그 코드 삭제

--- src/components/mypage/ScrollWrapper.tsx
+++ src/components/mypage/ScrollWrapper.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import * as S from './ScrollWrapper.styled';
 
 interface ScrollWrapperProps {
@@ -12,10 +11,6 @@ export default function ScrollWrapper({
   $height,
   scrollRef = null,
 }: ScrollWrapperProps) {
-  useEffect(() => {
-    console.log(scrollRef);
-  }, [scrollRef]);
-
   return (
     <S.ScrollWrapper $height={$height} ref={scrollRef}>
       {children}
