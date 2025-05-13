7734f87c07f4b1267ee2e7c7eea8dd0b87566b45
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2025-05-13 21:21:36 +0900

    design: 공고관리 카드 배치 수정

--- src/components/manageProjects/CardList.tsx
+++ src/components/manageProjects/CardList.tsx
@@ -4,7 +4,6 @@ import type { ManagedProject } from '../../models/manageMyProject';
 import Card from './Card';
 import CreateButton from '../../assets/createProjectButton.svg';
 import { ROUTES } from '../../constants/routes';
-import { Link } from 'react-router-dom';
 
 interface CardListProps {
   projects: ManagedProject[];
@@ -13,16 +12,19 @@ interface CardListProps {
 function CardList({ projects }: CardListProps) {
   return (
     <S.CardListWrapper>
-      <Link to={ROUTES.createProject}>
+      <S.MoveProjectLink to={ROUTES.createProject}>
         <S.CreateButton>
           <img src={CreateButton} />새 프로젝트 추가
         </S.CreateButton>
-      </Link>
+      </S.MoveProjectLink>
 
       {projects?.map((data) => (
-        <Link key={data.id} to={`${ROUTES.manageProjectsRoot}/${data.id}`}>
+        <S.MoveProjectLink
+          key={data.id}
+          to={`${ROUTES.manageProjectsRoot}/${data.id}`}
+        >
           <Card project={data} />
-        </Link>
+        </S.MoveProjectLink>
       ))}
     </S.CardListWrapper>
   );
