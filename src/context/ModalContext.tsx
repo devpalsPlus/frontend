4f27220053247c58ed7fc0eac4e4ca7a5a01a4bc
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-05-15 23:10:22 +0900

    feat : 모달 종료 시 콜백 함수를 실행할 수 있도록 수정

--- src/context/ModalContext.tsx
+++ src/context/ModalContext.tsx
@@ -3,11 +3,13 @@ import { createContext } from 'react';
 export interface ModalContextProps {
   isOpen: boolean;
   onClose: (event?: React.SyntheticEvent) => void;
+  onConfirm: (event?: React.SyntheticEvent) => void;
 }
 
 const defaultContext: Partial<ModalContextProps> = {
   isOpen: false,
   onClose: () => {},
+  onConfirm: () => {},
 };
 
 export const ModalContext =
