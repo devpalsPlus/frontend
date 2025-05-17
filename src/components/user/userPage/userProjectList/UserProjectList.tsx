fb47257ed82cad086c3fb8712fc17b680e206605
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2025-05-17 23:54:59 +0900

    refactor: 다른 유저페이지에 기획, 참여 프로젝트 분리, 커스텀훅에서 분기처리하여 api 사용하기

---
+++ src/components/user/userPage/userProjectList/UserProjectList.tsx
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import * as S from '../../mypage/joinedProject/MyJoinProjects.styled';
+import { ROUTES } from '../../../../constants/user/routes';
+import NoContent from '../../../common/noContent/NoContent';
+import ScrollWrapper from '../../mypage/ScrollWrapper';
+import Spinner from '../../mypage/Spinner';
+import Project from '../../mypage/joinedProject/Project';
+import { useGetUserProjectList } from '../../../../hooks/user/useGetUserProjectList';
+
+export default function UserProjects() {
+  const { userProjectData, isLoading, title } = useGetUserProjectList();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (!userProjectData) return;
+
+  console.log(userProjectData);
+  return (
+    <S.Container>
+      <S.FilterWrapper>
+        <S.FilterTitle>{title}</S.FilterTitle>
+      </S.FilterWrapper>
+      {userProjectData && userProjectData.length > 0 ? (
+        <ScrollWrapper>
+          <S.WrapperProject>
+            {userProjectData?.map((project) => (
+              <Link
+                key={project.id}
+                to={`${ROUTES.projectDetail}/${project.id}`}
+              >
+                <Project project={project} />
+              </Link>
+            ))}
+          </S.WrapperProject>
+        </ScrollWrapper>
+      ) : (
+        <S.NoWrapper>
+          <NoContent type='projects' />
+        </S.NoWrapper>
+      )}
+    </S.Container>
+  );
+}
