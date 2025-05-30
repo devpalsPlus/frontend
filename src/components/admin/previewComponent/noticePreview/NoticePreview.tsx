5b73b26cf2f6c7f94a0abbc848e2fb056c13596d
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-05-31 00:03:19 +0900

    feat : 공지사항 API 연결

---
+++ src/components/admin/previewComponent/noticePreview/NoticePreview.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import * as S from './NoticePreview.styled';
+import { useGetNotice } from '../../../../hooks/user/useGetNotice';
+import line from '../../../../assets/line.svg';
+
+const NoticePreview = () => {
+  const { noticeData } = useGetNotice({ keyword: '', page: 1 });
+
+  return (
+    <S.Container>
+      {noticeData?.notices.map((notice) => (
+        <S.Wrapper>
+          <S.Dot src={line} />
+          <S.NoticeTitle>{notice.title}</S.NoticeTitle>
+        </S.Wrapper>
+      ))}
+    </S.Container>
+  );
+};
+
+export default NoticePreview;
