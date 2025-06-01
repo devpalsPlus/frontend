eef03c48f4b409a82d89650a68c1b55c205a3af8
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-06-01 17:30:33 +0900

    feat : 문의 확인에 "상세 보기" 추가

--- src/components/admin/previewComponent/inquiresPreview/InquiresPreview.styled.ts
+++ src/components/admin/previewComponent/inquiresPreview/InquiresPreview.styled.ts
@@ -9,6 +9,12 @@ export const Container = styled.div`
 
 export const Wrapper = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
   align-items: center;
   margin-top: 16px;
 `;
@@ -18,12 +24,12 @@ export const Inquiry = styled(Link)`
 `;
 
 export const Category = styled.p`
-  font-size: 10px;
+  font-size: 9px;
   opacity: 50%;
 `;
 
 export const Title = styled.span`
-  font-size: 14px;
+  font-size: 13px;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
@@ -35,7 +41,7 @@ export const StateArea = styled.div`
 `;
 
 export const Date = styled.p`
-  font-size: 10px;
+  font-size: 9px;
   opacity: 50%;
 `;
 
@@ -46,6 +52,19 @@ export const Divider = styled.p`
 `;
 
 export const InquiryState = styled.p<{ $isCompleted: boolean }>`
-  font-size: 10px;
+  font-size: 9px;
   color: ${({ $isCompleted }) => ($isCompleted ? `#07DE00` : `#DE1A00`)};
 `;
+
+export const MoveToInquiryArea = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 10px;
+  margin-right: 10px;
+  margin-top: 20px;
+`;
+
+export const Text = styled.p``;
+
+export const Arrow = styled.img``;
--- src/components/admin/previewComponent/inquiresPreview/InquiresPreview.tsx
+++ src/components/admin/previewComponent/inquiresPreview/InquiresPreview.tsx
@@ -4,6 +4,7 @@ import { useGetAllInquiries } from '../../../../hooks/admin/useGetAllInquiries';
 import Avatar from '../../../common/avatar/Avatar';
 import { ADMIN_ROUTE } from '../../../../constants/routes';
 import { Link } from 'react-router-dom';
+import arrow_right from '../../../../assets/ArrowRight.svg';
 
 const InquiresPreview = () => {
   const { allInquiriesData } = useGetAllInquiries();
@@ -18,19 +19,25 @@ const InquiresPreview = () => {
     <S.Container>
       {previewList?.map((inquiry) => (
         <S.Wrapper key={inquiry.id}>
-          {/* <Link to={`${ADMIN_ROUTE.}`} */}
-          <Avatar image={inquiry.user.img} size='41px' />
-          <S.Inquiry to={`${ADMIN_ROUTE.inquiries}/${inquiry.id}`}>
-            <S.Category>{inquiry.category}</S.Category>
-            <S.Title>{inquiry.title}</S.Title>
-            <S.StateArea>
-              <S.Date>{inquiry.createdAt}</S.Date>
-              <S.Divider>|</S.Divider>
-              <S.InquiryState $isCompleted={inquiry.state}>
-                {inquiry.state ? '답변 완료' : '답변 대기 중'}
-              </S.InquiryState>
-            </S.StateArea>
-          </S.Inquiry>
+          <S.Content>
+            {/* <Link to={`${ADMIN_ROUTE.}`} */}
+            <Avatar image={inquiry.user.img} size='38px' />
+            <S.Inquiry to={`${ADMIN_ROUTE.inquiries}/${inquiry.id}`}>
+              <S.Category>{inquiry.category}</S.Category>
+              <S.Title>{inquiry.title}</S.Title>
+              <S.StateArea>
+                <S.Date>{inquiry.createdAt}</S.Date>
+                <S.Divider>|</S.Divider>
+                <S.InquiryState $isCompleted={inquiry.state}>
+                  {inquiry.state ? '답변 완료' : '답변 대기 중'}
+                </S.InquiryState>
+              </S.StateArea>
+            </S.Inquiry>
+          </S.Content>
+          <S.MoveToInquiryArea to={`${ADMIN_ROUTE.inquiries}/${inquiry.id}`}>
+            <S.Text>상세 보기</S.Text>
+            <S.Arrow src={arrow_right} />
+          </S.MoveToInquiryArea>
         </S.Wrapper>
       ))}
     </S.Container>
